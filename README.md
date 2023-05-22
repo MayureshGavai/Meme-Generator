1a784becae63173f6170646f7634fd2d44f460f0
Mayuresh Gavai <blackoutmayuresh2912@gmail.com>
2023-05-23 03:03:47 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Meme-Generator
+
+[Live Demo](https://musical-unicorn-f14c8b.netlify.app/)
+
+![Meme-Generator](./public/Meme%20Generator.png)
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@
 const header = () => {
   return (
     <header className="header">
-        <img src="src/assets/troll-face.png" alt="troll-face" />
+        <img src="./src/assets/troll-face.png" alt="troll-face" />
         <h2>Meme Generator</h2>
     </header>
 
